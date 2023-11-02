c83f3789fc26f2ae8b51581e01946ca13964f4a7
Asanali006 <148483489+Asanali006@users.noreply.github.com>
2023-11-02 09:23:24 +0600

    Add files via upload

---
+++ main15ЛАБ3.py
@@ -0,0 +1,8 @@
+radius = float(input("Введите радиус круга: "))
+
+area = 3.14159265 * radius ** 2
+
+circumference = 2 * 3.14 * radius
+
+print(f"Площадь круга с радиусом {radius} равна {area:.2f}")
+print(f"Длина окружности с радиусом {radius} равна {circumference:.2f}")
